16469c4b1d10ee0101cf011b47697a1a26a53113
Radoslaw Slowinski <radoslaw.slowinski@kainos.com>
2022-01-19 20:22:05 +0100

    wip

--- src/components/PublicProjectDetails.jsx
+++ src/components/PublicProjectDetails.jsx
@@ -1,8 +1,10 @@
-import {Card, Checkbox, Descriptions, Image, Typography} from "antd";
-import React, {useEffect, useState} from "react";
+import {Button, Card, Checkbox, Descriptions, Image, Typography} from "antd";
+import React, {useCallback, useEffect, useState} from "react";
 import {useMoralis} from "react-moralis";
 import {ReactComponent as PlusIcon} from "../plus.svg";
 import {useHistory, useLocation} from "react-router";
+import Account from "./Account/Account";
+import {CheckCircleOutlined, CloseCircleOutlined} from "@ant-design/icons";
 
 const {Text} = Typography;
 
@@ -35,27 +37,38 @@ const styles = {
 };
 
 export default function PublicProjectDetails(props) {
-    const {Moralis} = useMoralis();
-    const history = useHistory();
+    const {Moralis, isAuthenticated} = useMoralis();
     const projectId = props?.match?.params?.projectId
-    const [isLoading, setIsLoading] = useState(true)
     const [project, setProject] = useState();
+    const [isEligible, setIsEligible] = useState(false);
+    const [nothingFound, setNothingFound] = useState();
 
     useEffect(async () => {
         try {
-            const Project = Moralis.Object.extend("Project");
-            const query = new Moralis.Query(Project);
-            query.equalTo("isPublic", true)
-            query.get(projectId)
-            const result = await query.first()
-            setProject(result)
+            await fetchProject(projectId)
         } catch (e) {
             console.log(e)
+            setNothingFound(true)
         } finally {
-
         }
 
-    }, [props.match])
+    }, [props.match.params])
+
+    const fetchProject = useCallback(async () => {
+        const Project = Moralis.Object.extend("Project");
+        const query = new Moralis.Query(Project);
+        query.equalTo("isPublic", true)
+        query.equalTo("objectId", projectId)
+        const result = await query.first()
+        setProject(result)
+        await checkEligibility()
+    }, [])
+
+    const checkEligibility = useCallback(async () => {
+        const requiredNfts = await Moralis.Web3API.account.getNFTsForContract({token_address: project.get("requiredNftAddress")})
+        setIsEligible(requiredNfts.result.length > 0)
+
+    }, [project]);
 
     return (
         <div style={{display: "flex", flexDirection: "column", gap: "10px"}}>
@@ -66,7 +79,22 @@ export default function PublicProjectDetails(props) {
                     <Descriptions.Item label={"required nft"}>{project.get("requiredNftName")}</Descriptions.Item>
                 </Descriptions>
 
+                {!isAuthenticated &&
+                <span>Sign in with your wallet to check for eligibility:
+                    <div><Button><Account/></Button></div>
+                </span>}
+
+                {isAuthenticated &&
+                <div>
+                    Eligibility status:
+                    {!isEligible &&
+                    <CloseCircleOutlined style={{marginLeft: "0.5rem", fontSize: '20px', color: 'red'}}/>}
+                    {isEligible &&
+                    <CheckCircleOutlined style={{marginLeft: "0.5rem", fontSize: '20px', color: 'green'}}/>}
+                </div>}
+
             </Card>}
+            {!project && nothingFound && <h3>Nothing here...</h3>}
         </div>
     );
 }
