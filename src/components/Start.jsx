87694520108a9ed664eda7f7f3bdb17ce04bf967
Radoslaw Slowinski <radoslaw.slowinski@kainos.com>
2022-01-19 23:58:32 +0100

    start page

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,6 +1,5 @@
 import {Typography} from "antd";
-import React, {useEffect, useRef, useState} from "react";
-import {useMoralis} from "react-moralis";
+import React from "react";
 
 const {Text} = Typography;
 
@@ -35,12 +34,15 @@ const styles = {
 export default function Start() {
 
 
-
-
-
     return (
-        <div style={{display: "flex", flexDirection: "column", gap: "10px"}}>
-            Henlo
+        <div style={{display: "flex", flexDirection: "column", gap: "10px", justifyContent: "center"}}>
+            <span><strong>EVERSE</strong> aim is to revolutionize way of accessing digital content.</span>
+            <span>You can grant access for your Kajebi content based on NFT collection ownership of your choice.</span>
+            <span>TBD...</span>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
         </div>
     );
 }
