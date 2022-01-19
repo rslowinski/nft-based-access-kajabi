9e854d880d11bf2926b87aee92fbf5ac948b3330
Radoslaw Slowinski <radoslaw.slowinski@kainos.com>
2022-01-19 21:45:46 +0100

    add cloud functions

---
+++ moralis-cloud/clouds_functions.js
@@ -0,0 +1,55 @@
+Moralis.Cloud.define("HelloWorld", async(req) => {
+    const projectId = req.params.projectId;
+    const Project = Moralis.Object.extend("Project");
+    const query = new Moralis.Query(Project);
+    query.equalTo("isPublic", true)
+    query.equalTo("objectId", projectId)
+    const result = await query.first()
+
+    return {requiredNftAddress: result.get("requiredNftAddress"), requiredNftName: result.get("requiredNftName"), name: result.get("name")}
+});
+
+Moralis.Cloud.define("blah", async(req) => {
+    const projectId = req.params.projectId;
+    const userEmail = req.params.email;
+
+
+    const Project = Moralis.Object.extend("Project");
+    const query = new Moralis.Query(Project);
+    query.equalTo("isPublic", true)
+    query.equalTo("objectId", projectId)
+    const result = await query.first()
+
+    const userAddr = req.user.get("ethAddress")
+
+    const userNft = await Moralis.Web3API.account.getNFTsForContract({address: userAddr,token_address: result.get("requiredNftAddress")})
+
+    if(userNft.result.length <= 0){
+//  	return {result: (userAddr + " doesnt have required nft")}
+    }
+
+    const AccessMint = Moralis.Object.extend("AccessMint");
+    const accessMint = new AccessMint();
+    accessMint.set("ownerAddr", userAddr)
+    accessMint.set("ownerEmail", userEmail)
+    accessMint.set("project", result)
+    await accessMint.save()
+
+    const project = result;
+    Moralis.Cloud.httpRequest({
+        method: 'POST',
+        url: project.get("kajabiActivationUrl"),
+        body: {
+            email: userEmail,
+            name: userEmail,
+            external_user_id: userEmail
+        }
+    }).then(function(httpResponse) {
+        console.log(httpResponse.text);
+    }, function(httpResponse) {
+        console.error('Request failed with response code ' + httpResponse.status);
+        return httpRespose
+    });
+
+    return 200;
+});
